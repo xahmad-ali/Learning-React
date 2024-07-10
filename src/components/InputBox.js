92187bdec1243da4090df016003a936c25ac3767
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-07-10 14:06:13 +0500

    Currency_Exchanger  correction

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,43 +1,55 @@
-import React,{useId}from 'react'
+import React, { useId } from "react";
 
 function InputBox({
-    label,
-    amount,
-    onAmountChange,
-    onCurrencyChange,
-    currencyOption =[],
-    selectedCurrency ="usd",
-    className =""
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  selectedCurrency = "usd",
+  className = "",
+  amountDisable = false,
+  currencyDisable = false
 }) {
-
-    const id=useId();
+  const id = useId();
 
   return (
     <div className={` p-3 rounded-lg text-sm flex ${className}`}>
-        <div className='w-1/2'>
-            <label htmlFor={id} className='text-black/30 mb-2 inline-block'>{label}</label>
-            <input 
-            id={id}
-            type='number'
-            className='outline-none w-full bg-transparent py-1.5'
-            placeholder='Amount'
-            value={amount}
-            onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
-            />
-        </div>
-        <div className='w-1/2 flex flex-wrap justify-end text-right'>
-        <p className='text-black/40 mb-2 w-full'>Type</p>
-        <select className='cursor-pointer px-1 py-1 rounded-ld bg-gray-100'
-        value={selectedCurrency}
-        onChange={(e)=> onCurrencyChange && onCurrencyChange(Number(e.target.value))}
+      <div className="w-1/2">
+        <label htmlFor={id} className="text-black/30 mb-2 inline-block">
+          {label}
+        </label>
+        <input
+          id={id}
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          value={amount}
+          disabled={amountDisable}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Type</p>
+        <select
+          className="cursor-pointer px-1 py-1 rounded-ld bg-gray-100"
+          value={selectedCurrency}
+          disabled={false}
+          onChange={(e) =>
+            onCurrencyChange && onCurrencyChange(e.target.value)
+          }
         >
-            {currencyOption.map((currency)=>(
-                <option key={currency} value={currency}>{currency}</option>
-            ))}
+          {currencyOption.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default InputBox
+export default InputBox;
--- src/components/Screen4.js
+++ src/components/Screen4.js
@@ -4,11 +4,11 @@ import InputBox from "./InputBox";
 
 function Screen4() {
     const [amount,setAmount] =useState(0)
-    const [to,setTo] =useState("pkr")
     const [from,setFrom] =useState("usd")
+    const [to,setTo] =useState("pkr")
     const [convertedAmount,setConvertedAmount] =useState(0)
 
-    const currencyInfo = CurrenyApi(from) || {};
+    const currencyInfo = CurrenyApi(from)
     const option =Object.keys(currencyInfo)
 
     const convert=()=>{
@@ -30,15 +30,16 @@ function Screen4() {
     >
       <div className="w-full max-w-md mx-auto border border-gray-600 rounded-lg backdrop-blur-sm bg-white/30 p-4">
       <form onSubmit={(e)=> {
-        e.preventDefault()
-        convert()
+        e.preventDefault();
+        convert();
       }}>
         <div className="w-full mb-1">
-            <InputBox label="from"
+            <InputBox 
+            label="from"
             amount={amount} 
             currencyOption={option}
             onCurrencyChange={(currency)=> setFrom(currency)}
-            onAmountChange={(amount)=>setAmount(amount)}
+            onAmountChange={(amount)=> setAmount(amount)}
             selectedCurrency={from}
             />
         </div>
@@ -46,18 +47,24 @@ function Screen4() {
         <div className="relative w-full h-1.5 ">
             <button className="absolute left-1/2 translate-x-1/2 -translate-y-1/2
             border-2 border-white rounded-md bg-blue-600 text-white p-0.5" 
+            type="button"
             onClick={swap}> 
             Swap</button>
         </div>
         <div className="w-full mb-1">
-            <InputBox label="to"
+            <InputBox 
+            label="to"
             amount={convertedAmount} 
             currencyOption={option}
             onCurrencyChange={(currency)=> setTo(currency)}
-            onAmountChange={(amount)=>setAmount(amount)}
             selectedCurrency={to}
+            amountDisable //if nothing is passed with boolen by defualt it's true
             />
         </div>
+        <button 
+        type="submit"
+        className="bg-blue-600 w-full px-3 py-3 rounded-lg"
+        > Convert {from} to {to}</button>
       </form>
       </div>
     </div>
